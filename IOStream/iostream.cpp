3d0b24d031c34d38f7e46ed02fab4ab0524be955
Adetomiwa Adesanya <adetomiwaadesanya01@gmail.com>
2024-01-06 18:21:07 +0100

    fixed typo in iostream.cpp notes

--- IOStream/iostream.cpp
+++ IOStream/iostream.cpp
@@ -15,13 +15,13 @@ int main() {
     ====================
     NOTES
     ====================
-    
-    The std::cout is a buffered i.e the requests to it are stored somewhere in the memory, before the it is being flushed.
+
+    The std::cout is a buffered i.e the requests to it are stored somewhere in the memory, before it is being flushed.
 
     Flushed means that all the data collected in the buffer is transferred to its destination (in the case, the console).
 
     Best practice is to use '\n' over 'std::endl' when printing text to the console.
-        - std::endl is quite inefficient because it actually does two jobs: it moves the console to the next line,
+        - std::endl is quite inefficient because it actually does two jobs: it moves the cursor to the next line,
         and it flushes the buffer.
         - \n only moves the cursor to the next line and doesn't make any request to flush the buffer.
 */
