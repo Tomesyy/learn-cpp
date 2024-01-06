5cbe6592bc843be1f4ff8df88e09f3ab80f9aaea
Adetomiwa Adesanya <adetomiwaadesanya01@gmail.com>
2024-01-06 18:19:38 +0100

    add notes to cpp files

--- IOStream/iostream.cpp
+++ IOStream/iostream.cpp
@@ -10,3 +10,18 @@ int main() {
 
     return 0;
 }
+
+/*
+    ====================
+    NOTES
+    ====================
+    
+    The std::cout is a buffered i.e the requests to it are stored somewhere in the memory, before the it is being flushed.
+
+    Flushed means that all the data collected in the buffer is transferred to its destination (in the case, the console).
+
+    Best practice is to use '\n' over 'std::endl' when printing text to the console.
+        - std::endl is quite inefficient because it actually does two jobs: it moves the console to the next line,
+        and it flushes the buffer.
+        - \n only moves the cursor to the next line and doesn't make any request to flush the buffer.
+*/
--- UnintializedVariables/uninitializedvariables.cpp
+++ UnintializedVariables/uninitializedvariables.cpp
@@ -7,3 +7,12 @@ int main() {
 
     return 0;
 }
+
+/*
+    ====================
+    NOTES
+    ====================
+    
+    Since we didn't assign any value to x (x is uninitialized), it is going to be assigned to a random unused memory address,
+    hence printing different random number in the console on each execution.
+*/
